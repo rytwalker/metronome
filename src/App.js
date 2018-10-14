9ef1fc980d37b8d01c25d8a3f93db1bf01d2ba24
rytwalker <rytwalker@gmail.com>
2018-10-14 05:08:23 -0400

    Basic functionality added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,86 @@
 import React, { Component } from 'react';
 import './App.css';
+import click1 from './click1.wav';
+import click2 from './click2.wav';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       bpm: 100,
-      isPlaying: false
+      isPlaying: false,
+      count: 0,
+      beatsPerMeasure: 4
     };
+    this.click1 = new Audio(click1);
+    this.click2 = new Audio(click2);
   }
+
+  playClick = () => {
+    const { count, beatsPerMeasure } = this.state;
+
+    // Give first beat different sound
+    if (count % beatsPerMeasure === 0) {
+      this.click2.play();
+    } else {
+      this.click1.play();
+    }
+
+    // Keep track of what beat we're on
+    this.setState(prevState => ({
+      count: (prevState.count + 1) % prevState.beatsPerMeasure
+    }));
+  };
+
+  handleBpmChange = e => {
+    const { isPlaying } = this.state;
+    const bpm = e.target.value;
+    const interval = (60 / bpm) * 1000;
+
+    if (isPlaying) {
+      // Stop old timer and start new timer
+      clearInterval(this.timer);
+      this.timer = setInterval(this.playClick, interval);
+      // Set new BPM and reset beat counter
+      this.setState({ counter: 0, bpm });
+    } else {
+      // Just update bpm
+      this.setState({ bpm });
+    }
+  };
+
+  toggleMetronomeClick = () => {
+    const { isPlaying, bpm } = this.state;
+    const interval = (60 / bpm) * 1000;
+
+    if (isPlaying) {
+      clearInterval(this.timer);
+      this.setState({ isPlaying: false });
+    } else {
+      // Start Timer
+      this.timer = setInterval(this.playClick, interval);
+      // setState
+      this.setState({ count: 0, isPlaying: true }, this.playClick);
+    }
+  };
+
   render() {
     const { bpm, isPlaying } = this.state;
     return (
       <div className="App">
         <h1 className="title">Metronome</h1>
         <div className="bpm">{bpm} BPM</div>
-        <input className="slider" type="range" min="60" max="240" value={bpm} />
-        <button className="btn">{isPlaying ? 'Stop' : 'Start'}</button>
+        <input
+          className="slider"
+          type="range"
+          min="60"
+          max="240"
+          value={bpm}
+          onChange={this.handleBpmChange}
+        />
+        <button className="btn" onClick={this.toggleMetronomeClick}>
+          {isPlaying ? 'Stop' : 'Start'}
+        </button>
       </div>
     );
   }
