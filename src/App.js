da53466bf8958d7ed31f01c63ab1a2001bc58285
rytwalker <rytwalker@gmail.com>
2018-10-14 04:37:07 -0400

    Basic UI constructed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      bpm: 100,
+      isPlaying: false
+    };
+  }
   render() {
+    const { bpm, isPlaying } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 className="title">Metronome</h1>
+        <div className="bpm">{bpm} BPM</div>
+        <input className="slider" type="range" min="60" max="240" value={bpm} />
+        <button className="btn">{isPlaying ? 'Stop' : 'Start'}</button>
       </div>
     );
   }
